b40ca1180135c7ec8a200a25769d8a330242f3b7
niiyama-kota <k0o4t2a5@gmail.com>
2022-11-21 15:57:41 +0900

    modify members

--- src/data_structure/btree.rs
+++ src/data_structure/btree.rs
@@ -1,19 +1,20 @@
 pub mod btree
 {
-    struct BTreeNode<'a, T: std::cmp::Ord + std::default::Default + 'a, const B: usize>
+    #[derive(Clone)]
+    struct BTreeNode<T: std::cmp::Ord + std::default::Default + Clone, const B: usize>
     {
         _keys: Vec<T>,
-        _child: &'a  Vec::<BTreeNode<'a, T, B>>,
+        _child: Option<Vec::<BTreeNode<T, B>>>,
         _size: usize,
         _leaf: bool
     }
 
-    impl<'a, T: std::cmp::Ord + std::default::Default + 'a, const B: usize> BTreeNode<'a, T, B>
+    impl<T: std::cmp::Ord + std::default::Default + Clone, const B: usize> BTreeNode<T, B>
     {
         pub fn new(leaf: bool) -> Self
         {
-            let mut keys = vec![Default::default();2 * B - 1];
-            let mut child = &vec![BTreeNode::<'a, T, B>::new(true);2*B];
+            let keys = vec![Default::default();2 * B - 1];
+            let child = if leaf {None} else {Some(vec![BTreeNode::<T, B>::new(true);2*B])};
             let size = 0 as usize;
             Self
             {
